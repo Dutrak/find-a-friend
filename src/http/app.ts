fee824850438702ff634f763321884476e514fd9
Dutrak <fernandodutra100701@gmail.com>
2024-07-31 22:30:10 -0300

    feat:add app.ts plugins

--- src/http/app.ts
+++ src/http/app.ts
@@ -6,3 +6,24 @@ import { OrgRoutes } from './controllers/orgs/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(OrgRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // TODO: Log to external tool like Datadog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
